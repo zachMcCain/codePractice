9fa660cefb06fd1113e17d7c1f4711024b2a7eca
Zachary McCain <zacmmcca@gmail.com>
2021-03-04 15:26:53 -0600

    Completed sameFrequency

--- algoDataStructures/problemSolvingPatterns/sameFrequenc/sameFrequency.js
+++ algoDataStructures/problemSolvingPatterns/sameFrequenc/sameFrequency.js
@@ -12,6 +12,39 @@
  */
 
 
-function sameFrequency() {
+function sameFrequency(n1, n2) {
+  let string1 = n1.toString()
+  let string2 = n2.toString()
+  let digits = {};
 
+  for (let i = 0; i < string1.length; i++) {
+    let digit = string1[i]
+    if (digits[digit]) {
+      digits[digit] = digits[digit] + 1;
+    } else {
+      digits[digit] = 1;
+    }
+  }
+
+  for (let j = 0; j < string2.length; j++) {
+    let digit = string2[j]
+    if (digits[digit]) {
+      digits[digit] = digits[digit] - 1;
+    } else {
+      return false;
+    }
+  }
+
+  let keys = Object.keys(digits);
+  for (let k = 0; k < keys.length; k++) {
+    let digit = keys[k];
+    if (digits[digit]) {
+      return false;
+    }
+  }
+  console.log(digits);
+  return true;
 }
+
+console.log(sameFrequency(182, 281))
+console.log(sameFrequency(123, 123254))
