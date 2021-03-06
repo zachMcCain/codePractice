a69762916e16af7cc9dbf55c3993e369d8f3805b
Zachary McCain <zacmmcca@gmail.com>
2021-03-05 20:01:30 -0600

    Solved areThereDuplicates three ways.

--- algoDataStructures/problemSolvingPatterns/areThereDuplicates/areThereDuplicates.js
+++ algoDataStructures/problemSolvingPatterns/areThereDuplicates/areThereDuplicates.js
@@ -16,6 +16,40 @@
  * Space - O(1)
  */
 
- function areThereDuplicates() {
+ // Simple solution:
+ // Iterate through the arguments
+ // Check to see if the argument is in object
+ // Save each element in an object
 
- }
+function areThereDuplicatesCounter() {
+  let count = {};
+  for (let i = 0; i < arguments.length; i++) {
+    if (count[arguments[i]]) {
+      return true;
+    }
+    count[arguments[i]] = 1;
+  }
+  return false;
+}
+
+function areThereDuplicatesPointers(...args) {
+  console.log(Array.isArray(arguments));
+  console.log(Array.isArray(args));
+  let sorted = args.sort((a, b) => a < b)
+  let firstPointer = sorted[0]
+  for (let i = 1; i < sorted.length; i++) {
+    if (sorted[i] === firstPointer) {
+      return true;
+    }
+    firstPointer = sorted[i];
+  }
+  return false;
+}
+
+function areThereDuplicatesSet() {
+  return new Set(arguments).size !== arguments.length;
+}
+
+console.log(areThereDuplicatesCounter(1, 2, 3, 4, 3)); // true
+console.log(areThereDuplicatesCounter(1, 2, 4, 4)); // true
+console.log(areThereDuplicatesCounter(12, 324, 5)); // false
